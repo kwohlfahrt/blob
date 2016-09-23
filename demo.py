8bd936e927504d35a3aad3189b66673533c19121
Kai Wohlfahrt <kjw53@cam.ac.uk>
2016-09-23 11:34:41 +0100

    Add demo image
    Removes scikit-image dependency.

--- demo.py
+++ demo.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
-from skimage.data import hubble_deep_field
 from matplotlib import pyplot as plt
 import blob
+from pathlib import Path
+from tifffile import imread
 
-data = hubble_deep_field().astype('float32').sum(axis=-1)
+data_path = Path(__file__).parent / "tests" / "fixtures" / "hubble_deep_field.tif"
+data = imread(str(data_path)).astype('float32').sum(axis=-1)
 
-# Equivalent to `blobs --size 3 20 --threshold 5 hubble_deep_field.tif`
+# Equivalent to `blobs --size 5 10 --threshold 10 hubble_deep_field.tif`
 blobs = blob.findBlobs(data, scales=range(5, 10), threshold=10)
 
 fig, ax = plt.subplots(1, 1)
